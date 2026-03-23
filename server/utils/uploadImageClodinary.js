ccecde3f7f44c10cf38b4b5760d98da70805fa7d
Senator Lambi <beyoh172@gmail.com>
2026-03-23 18:36:07 +0100

    fixed conflicting cloudinary keys

--- server/utils/uploadImageClodinary.js
+++ server/utils/uploadImageClodinary.js
@@ -1,21 +1,26 @@
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-    cloud_name : process.env.NEW_CLODINARY_CLOUD_NAME,
-    api_key : process.env.NEW_CLODINARY_API_KEY,
-    api_secret : process.env.NEW_CLODINARY_API_SECRET_KEY
-})
+  cloud_name: process.env.CLODINARY_CLOUD_NAME,
+  api_key: process.env.CLODINARY_API_KEY,
+  api_secret: process.env.CLODINARY_API_SECRET_KEY,
+});
 
-const uploadImageClodinary = async(image)=>{
-    const buffer = image?.buffer || Buffer.from(await image.arrayBuffer())
+const uploadImageClodinary = async (image) => {
+  const buffer = image?.buffer || Buffer.from(await image.arrayBuffer());
 
-    const uploadImage = await new Promise((resolve,reject)=>{
-        cloudinary.uploader.upload_stream({ folder : "essentialist makeup store"},(error,uploadResult)=>{
-            return resolve(uploadResult)
-        }).end(buffer)
-    })
+  const uploadImage = await new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        { folder: "essentialist makeup store" },
+        (error, uploadResult) => {
+          return resolve(uploadResult);
+        },
+      )
+      .end(buffer);
+  });
 
-    return uploadImage
-}
+  return uploadImage;
+};
 
-export default uploadImageClodinary
+export default uploadImageClodinary;
